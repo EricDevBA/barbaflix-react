5a595a3191d90fc81ec76818451678fd9c1defdf
Santos <eric.b.santos@capgemini.com>
2020-07-29 17:28:09 -0300

    Building page cadastro and refatoring with JS e CSS

--- src/index.js
+++ src/index.js
@@ -6,16 +6,19 @@ import './index.css';
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home/Index';
-import CadastroVideo from './pages/cadastro/Video';
+import CadastroVideo from './pages/cadastro';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
-
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 
 ReactDOM.render(
   <BrowserRouter>
   <Switch>
-  <Route path="/" component={Home} exact/>
-  <Route path="/cadastro/video" component={CadastroVideo}/>
+    <Route path="/" component={Home} exact/>
+    <Route path="/cadastro/video" component={CadastroVideo}/>
+    <Route path="/cadastro/categoria" component={CadastroCategoria}/>
+    <Route component={Pagina404}/>
   </Switch>
   </BrowserRouter>,
   document.getElementById('root')
